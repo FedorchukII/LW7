c46f6f96cd5683440669d9fe9eaee400e68602d9
FedorchukII <164139022+FedorchukII@users.noreply.github.com>
2024-05-29 09:23:15 +0300

    Add files via upload

--- 10.py
+++ 10.py
@@ -1,9 +1,19 @@
 """FizzBuzz: Напишите программу, которая выводит числа от 1 до 100.
 Но для чисел, кратных трём, вместо числа выведите “Fizz”, а для чисел, кратных пяти, выведите “Buzz”.
 Для чисел, кратных как трём, так и пяти, выведите “FizzBuzz”."""
+three = 3
+five = 5
 
-
-for i in range(1, 101):
-    if i % 3 == 0:
-        counter += 1
-
+for i in range(1,101):
+    if i == three and i != five:
+        print("Fizz")
+        three += 3
+    elif i != three and i == five:
+        print("Buzz")
+        five += 5
+    elif i == three and i == five:
+        print("FizzBuzz")
+        three += 3
+        five += 5
+    else:
+        print(i)
